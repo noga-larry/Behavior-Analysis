5263384d093a6adc0d82b7c5a6c93df8be99b98b
nogalarry <noga.larry@mail.huji.ac.il>
2020-06-25 09:54:20 +0300

    calculate length, overshoot and velocity

--- saccadeRTs.m
+++ saccadeRTs.m
@@ -1,4 +1,4 @@
-function RT = saccadeRTs(data,ind)
+function [RT,Len,OverShoot,Vel] = saccadeRTs(data,ind)
 
 
 minRT = 20; % minimal time between traget movemet and the saccade;
@@ -6,6 +6,11 @@ maxRT = 800; % maximal time between traget movemet and the saccade;
 minNorm = 4; % minimal norm of saccade
 
 RT = nan(1,length(ind));
+Len = nan(1,length(ind));
+OverShoot = nan(1,length(ind));
+Vel = nan(1,length(ind));
+
+[~,match_d] = getDirections(data);
 for t = 1:length(ind)
 
  
@@ -13,16 +18,23 @@ for t = 1:length(ind)
     saccadeWindowEnd = data.trials(ind(t)).movement_onset + maxRT;
     saccadeIsTimed = (saccadeWindowBegin < data.trials(ind(t)).beginSaccade)...
         & (saccadeWindowEnd > data.trials(ind(t)).beginSaccade);
-    deltaH = data.trials(ind(t)).hPos(data.trials(ind(t)).beginSaccade)...
-        - data.trials(ind(t)).hPos(data.trials(ind(t)).endSaccade);
-    deltaV = data.trials(ind(t)).vPos(data.trials(ind(t)).beginSaccade)...
-        - data.trials(ind(t)).vPos(data.trials(ind(t)).endSaccade);
+    deltaH = data.trials(ind(t)).hPos(data.trials(ind(t)).endSaccade)...
+        - data.trials(ind(t)).hPos(data.trials(ind(t)).beginSaccade);
+    deltaV = data.trials(ind(t)).vPos(data.trials(ind(t)).endSaccade)...
+        - data.trials(ind(t)).vPos(data.trials(ind(t)).beginSaccade);
     saccadeNormIsLarge = (vecnorm([deltaH;deltaV])>minNorm);
   
     saccInd = find (saccadeNormIsLarge & saccadeIsTimed);
     if length(saccInd)==0
         continue
     end
+    
+    posChangeInTargetDirection = rotateEyeMovement(deltaH(saccInd(1)),deltaV(saccInd(1)),...
+        -match_d(ind(t))-data.trials(ind(t)).screen_rotation);
     RT(t) = data.trials(ind(t)).beginSaccade(saccInd(1))-data.trials(ind(t)).movement_onset;
-
+    Len(t) = data.trials(ind(t)).endSaccade(saccInd(1))-data.trials(ind(t)).beginSaccade(saccInd(1));
+    Vel(t) = (posChangeInTargetDirection/Len(t))*1000;
+    OverShoot(t) = rotateEyeMovement(data.trials(ind(t)).hPos(data.trials(ind(t)).endSaccade(saccInd(1)))...
+        ,data.trials(ind(t)).vPos(data.trials(ind(t)).endSaccade(saccInd(1))),...
+        -match_d(ind(t))-data.trials(ind(t)).screen_rotation);
 end

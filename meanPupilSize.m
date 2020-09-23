3709d219874f71b0139a2c8dfe39d6f30bef77be
nogalarry <noga.larry@mail.huji.ac.il>
2020-09-23 16:36:41 +0300

    meanPupilSize: remove saccades + smooth

--- meanPupilSize.m
+++ meanPupilSize.m
@@ -3,25 +3,24 @@ function [avePupil,pupil] = meanPupilSize(data,ind,params)
 LEN = params.time_before + params.time_after+1;
 pupil = nan(LEN,length(ind));
 
-
-switch params.align_to
-    case 'cue'
-        
-        pupil = zeros (LEN,length(ind));
-        
-        for f = 1:length(ind)
+for f = 1:length(ind)
+    
+    pupil_t = data.trials(ind(f)).pupil;
+    
+    pupil_t = removesSaccades(pupil_t,data.trials(ind(f)).beginSaccade,data.trials(ind(f)).endSaccade );
+    pupil_t = removesSaccades(pupil_t,data.trials(ind(f)).blinkBegin,data.trials(ind(f)).blinkEnd);
+    
+    switch params.align_to        
+        case 'cue'
             ts = data.trials(ind(f)).cue_onset + ((-params.time_before):params.time_after);
-            pupil_t = data.trials(ind(f)).pupil(ts);
-            pupil (:,f) = pupil_t;
-        end
-        
-    case 'targetMovementOnset'
-        for f = 1:length(ind)
-            ts = data.trials(ind(f)).movement_onset +((-params.time_before):params.time_after);
-            pupil_t = data.trials(ind(f)).pupil(ts);
-            pupil (:,f) = pupil_t;
-        end       
-
+        case 'targetMovementOnset'
+            ts = data.trials(ind(f)).movement_onset +((-params.time_before):params.time_after);            
+    end
+   
+    pupil_t = pupil_t(ts);
+    pupil (:,f) = pupil_t;
+            
 end
 
-avePupil = mean(pupil,2);
+avePupil = nanmean(pupil,2);
+avePupil = gaussSmooth(avePupil,params.SD);
